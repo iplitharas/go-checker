ecf52f3be0599e43554fd550900756a8b9f44f7e
iplitharas <iplitharas@core-innovation.com>
2024-03-26 21:53:36 +0200

    add banner at readme

---
+++ cmd/checker/flags.go
@@ -0,0 +1,11 @@
+package main
+
+type flags struct {
+	url  string
+	n, c int
+}
+
+func (f *flags) parse() error {
+	// parsing loging
+	return nil
+}
--- cmd/checker/main.go
+++ cmd/checker/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 const (
 	bannerText = `
@@ -26,6 +29,13 @@ func usage() string  { return usageText[1:] }
 
 func main() {
 	fmt.Println("Checker entrypoint!")
+
+	var f flags
+	if err := f.parse(); err != nil {
+		fmt.Println(usage())
+		log.Fatal(err)
+	}
 	fmt.Println(banner())
-	fmt.Println(usage())
+	fmt.Printf("Making %d requests to %s with a concurrency level of %d.\n",
+		f.n, f.url, f.c)
 }
