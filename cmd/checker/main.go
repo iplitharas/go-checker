b23c67284d52cda25c2e7406c6e4c763b4cbc990
iplitharas <iplitharas@core-innovation.com>
2024-03-26 21:40:00 +0200

    make banner text and promt read only

--- cmd/checker/main.go
+++ cmd/checker/main.go
@@ -2,15 +2,30 @@ package main
 
 import "fmt"
 
-var bannerText = `
+const (
+	bannerText = `
 __        __   _        ____ _               _             
 \ \      / /__| |__    / ___| |__   ___  ___| | _____ _ __ 
  \ \ /\ / / _ \ '_ \  | |   | '_ \ / _ \/ __| |/ / _ \ '__|
   \ V  V /  __/ |_) | | |___| | | |  __/ (__|   <  __/ |   
    \_/\_/ \___|_.__/   \____|_| |_|\___|\___|_|\_\___|_|   
-`[1:]
+`
+	usageText = `
+Usage:
+  -url
+       HTTP server URL to make requests (required)
+  -n
+       Number of requests to make
+  -c
+       Concurrency level
+`
+)
+
+func banner() string { return bannerText[1:] }
+func usage() string  { return usageText[1:] }
 
 func main() {
 	fmt.Println("Checker entrypoint!")
-	fmt.Println(bannerText)
+	fmt.Println(banner())
+	fmt.Println(usage())
 }
